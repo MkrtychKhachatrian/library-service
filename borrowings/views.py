e1e87bfa8c136f81778ff0840c9b64a9f4ed7ee9
Mkrtych Khachatrian <mukuchbrat@gmail.com>
2023-07-06 18:02:56 +0300

    Add BorrowingViewSet & urls

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from borrowings.models import Borrowing
+from borrowings.serializers import BorrowingSerializer, BorrowingDetailSerializer
+
+
+class BorrowingViewSet(viewsets.ModelViewSet):
+    queryset = Borrowing.objects.all()
+    serializer_class = BorrowingSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list" or self.action == "retrieve":
+            return BorrowingDetailSerializer
+
+        return BorrowingSerializer
